a0f86f3517458b132da6e226f9d4736f1f3ef180
Mathias Bynens <mathias@qiwi.be>
2013-02-20 17:53:19 +0100

    Support Grunt v0.4.0’s `Task#files`
    See http://gruntjs.com/api/inside-tasks#inside-multi-tasks.

--- tasks/yui-compressor.js
+++ tasks/yui-compressor.js
@@ -3,20 +3,40 @@ module.exports = function(grunt) {
 	var yuiCompressor = require('./lib/yui-compressor.js').init(grunt);
 
 	grunt.registerMultiTask('min', 'Minify JavaScript files with YUI Compressor.', function() {
-		yuiCompressor({
-			'type': 'js',
-			'source': this.file.src,
-			'destination': this.file.dest,
-			'fn': this.async()
+		var done = this.async();
+		var files = this.files;
+		var length = files.length;
+		var count = 0;
+		files.forEach(function(file) {
+			yuiCompressor({
+				'type': 'js',
+				'source': file.src,
+				'destination': file.dest,
+				'fn': function() {
+					if (++count == length) {
+						done();
+					}
+				}
+			});
 		});
 	});
 
 	grunt.registerMultiTask('cssmin', 'Minify CSS files with YUI Compressor.', function() {
-		yuiCompressor({
-			'type': 'css',
-			'source': this.file.src,
-			'destination': this.file.dest,
-			'fn': this.async()
+		var done = this.async();
+		var files = this.files;
+		var length = files.length;
+		var count = 0;
+		files.forEach(function(file) {
+			yuiCompressor({
+				'type': 'css',
+				'source': file.src,
+				'destination': file.dest,
+				'fn': function() {
+					if (++count == length) {
+						done();
+					}
+				}
+			});
 		});
 	});
 
