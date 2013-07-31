a16064e10594776845c4bcb32bc6ce92fd36f5f7
Mathias Bynens <mathias@qiwi.be>
2013-07-31 22:42:55 +0200

    Clean up tasks/lib/yui-compressor.js

--- tasks/lib/yui-compressor.js
+++ tasks/lib/yui-compressor.js
@@ -4,25 +4,24 @@ exports.init = function(grunt) {
 
 	// Refactored out the `min_max_info` and `concat` helpers here
 	// because `grunt.helper` is removed in future Grunt versions
-	var minMax = require('grunt-lib-contrib').init(grunt).minMaxInfo,
-		concat = function(source) {
-			// Kinda hacky, but that’s how I roll…
-			return source.map(function(filepath) {
-				return grunt.file.read(filepath);
-			}).join('\n');
-		};
+	var minMax = require('grunt-lib-contrib').init(grunt).minMaxInfo;
+	var concat = function(source) {
+		// Kinda hacky, but that’s how I roll…
+		return source.map(function(filepath) {
+			return grunt.file.read(filepath);
+		}).join('\n');
+	};
 
 	return function(options) {
-		var source = grunt.file.expand(options.source),
-			destination = options.destination,
-			max = concat(source),
-			min,
-			report = options.report;
+		var source = grunt.file.expand(options.source);
+		var destination = options.destination;
+		var max = concat(source);
+		var min;
+		var report = options.report;
 
-
-		if (source.indexOf(destination) === -1) {
-			// Ugly hack to create the destination path automatically if needed
-		   grunt.file.write(destination, '');
+		// Ugly hack to create the destination path automatically if needed
+		if (!grunt.file.exists(destination)) {
+			grunt.file.write(destination, '');
 		}
 
 		// Minify all the things!
