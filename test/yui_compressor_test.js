0829b0583688416fddfe25e688fd5dcdbdce73b9
Fred Blundun <fred@snowplowanalytics.com>
2014-02-04 11:46:51 +0000

    Changed tests to use tabs rather than spaces

--- test/yui_compressor_test.js
+++ test/yui_compressor_test.js
@@ -3,44 +3,47 @@
 var grunt = require('grunt');
 
 exports.yui_compressor = {
-  setUp: function (done) {
-    // setup here if necessary
-    done();
-  },
-  min_simple: function (test) {
-    test.expect(1);
+	setUp: function (done) {
+		// setup here if necessary 
+		done();
+	},
 
-    var actual = grunt.file.read('tmp/simple.min.js');
-    var expected = grunt.file.read('test/expected/simple.min.js');
-    test.equal(actual, expected, 'should minify a JavaScript file correctly.');
+	min_simple: function (test) {
+		test.expect(1);
 
-    test.done();
-  },
-  min_w_breaks: function (test) {
-    test.expect(1);
+		var actual = grunt.file.read('tmp/simple.min.js');
+		var expected = grunt.file.read('test/expected/simple.min.js');
+		test.equal(actual, expected, 'should minify a JavaScript file correctly.');
 
-    var actual = grunt.file.read('tmp/w_breaks.min.js');
-    var expected = grunt.file.read('test/expected/w_breaks.min.js');
-    test.equal(actual, expected, 'should minify a JavaScript file using the line break option correctly.');
+		test.done();
+	},
 
-    test.done();
-  },
-  cssmin_simple: function (test) {
-    test.expect(1);
+	min_w_breaks: function (test) {
+		test.expect(1);
 
-    var actual = grunt.file.read('tmp/simple.min.css');
-    var expected = grunt.file.read('test/expected/simple.min.css');
-    test.equal(actual, expected, 'should minify a CSS file correctly.');
+		var actual = grunt.file.read('tmp/w_breaks.min.js');
+		var expected = grunt.file.read('test/expected/w_breaks.min.js');
+		test.equal(actual, expected, 'should minify a JavaScript file using the line break option correctly.');
 
-    test.done();
-  },
-    cssmin_w_breaks: function (test) {
-    test.expect(1);
+		test.done();
+	},
+	cssmin_simple: function (test) {
+		test.expect(1);
 
-    var actual = grunt.file.read('tmp/w_breaks.min.css');
-    var expected = grunt.file.read('test/expected/w_breaks.min.css');
-    test.equal(actual, expected, 'should minify a CSS file correctly.');
+		var actual = grunt.file.read('tmp/simple.min.css');
+		var expected = grunt.file.read('test/expected/simple.min.css');
+		test.equal(actual, expected, 'should minify a CSS file correctly.');
 
-    test.done();
-  }
+		test.done();
+	},
+
+	cssmin_w_breaks: function (test) {
+		test.expect(1);
+
+		var actual = grunt.file.read('tmp/w_breaks.min.css');
+		var expected = grunt.file.read('test/expected/w_breaks.min.css');
+		test.equal(actual, expected, 'should minify a CSS file using the line break option correctly.');
+
+		test.done();
+	}
 };
